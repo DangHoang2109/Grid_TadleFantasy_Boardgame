9c0157d86ed9bc5ef9fd06e583234930ed1b92ba
danghoang2109 <danghoang2109@gmail.com>
2024-12-17 20:54:07 +0700

    split function move into another function and callback

--- Boardgame_TaddleFantasy/Assets/Games/Scripts/PlayerUnit.cs
+++ Boardgame_TaddleFantasy/Assets/Games/Scripts/PlayerUnit.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class PlayerUnit : Unit
 {
+    public static System.Action OnPlayerMove;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +22,14 @@ public class PlayerUnit : Unit
 
     void OnNodeClicked(NodeBase nodeClicked)
     {
-        this.transform.DOJump(nodeClicked.transform.position, 0.5f, 1, 0.2f).SetEase(Ease.InQuad);
-        if (nodeClicked is SquareTileOnBoardNode tileNode)
+        MoveToNode(nodeClicked);
+    }
+    public virtual void MoveToNode(NodeBase nodeDestination)
+    {
+        this.transform.DOJump(nodeDestination.transform.position, 0.5f, 1, 0.2f).SetEase(Ease.InQuad);
+        if (nodeDestination is SquareTileOnBoardNode tileNode)
             tileNode.Flip();
+
+        OnPlayerMove?.Invoke();
     }
 }
